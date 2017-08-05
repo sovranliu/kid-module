e296321ac5214c01c66299cefa1a709cc3c7c0ca
sovranliu <sovranliu@aliyun.com>
2017-08-06 00:54:29 +0800

    简单修改

--- kid-logic/kid-logic-biz/src/main/java/com/xyzq/kid/logic/pay/PayListenerService.java
+++ kid-logic/kid-logic-biz/src/main/java/com/xyzq/kid/logic/pay/PayListenerService.java
@@ -5,27 +5,45 @@ import com.xyzq.kid.logic.config.service.GoodsTypeService;
 import com.xyzq.kid.logic.record.service.RecordService;
 import com.xyzq.kid.logic.ticket.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
- * Created by hedongyang
- * DATE @ 2017/8/6.
+ * 支付监听器
  */
+@Service("payListener")
 public class PayListenerService implements PayListener {
+    /**
+     * 票务服务
+     */
 	@Autowired
 	private TicketService ticketService;
+    /**
+     * 飞行日志服务
+     */
 	@Autowired
 	private RecordService recordService;
 	/**
-	 * 票价服务
+	 * 商品类型服务
 	 */
 	@Autowired
 	private GoodsTypeService goodsTypeService;
 
+
+    /**
+     * 支付回调
+     *
+     * @param orderNo 订单号
+     * @param openId 微信用户开放ID
+     * @param goodsType 商品类型
+     * @param fee 支付金额
+     * @param tag 附属数据
+     */
 	@Override
 	public void onPay(String orderNo, String openId, int goodsType, int fee, String tag) {
 		if (goodsTypeService.isRecord(goodsType)) {
 			recordService.onPay(orderNo, openId, goodsType, fee, tag);
-		} else {
+		}
+		else {
 			ticketService.onPay(orderNo, openId, goodsType, fee, tag);
 		}
 	}
