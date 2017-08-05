bf15a6e394a948396acec10240709238c28581c0
sovranliu <sovranliu@aliyun.com>
2017-08-05 19:21:20 +0800

    新增管理员服务会话方法

--- kid-logic/kid-logic-biz/src/main/java/com/xyzq/kid/logic/admin/service/AdminService.java
+++ kid-logic/kid-logic-biz/src/main/java/com/xyzq/kid/logic/admin/service/AdminService.java
@@ -3,16 +3,31 @@ package com.xyzq.kid.logic.admin.service;
 import com.xyzq.kid.logic.admin.bean.AdminBean;
 import com.xyzq.kid.logic.admin.dao.po.AdminPO;
 import com.xyzq.kid.logic.admin.entity.AdminEntity;
+import com.xyzq.simpson.base.etc.Serial;
+import com.xyzq.simpson.utility.cache.core.ITimeLimitedCache;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.logging.Logger;
+import javax.annotation.Resource;
 
 /**
- * Created by Brann on 17/7/27.
+ * 管理员服务
  */
 @Service("adminService")
 public class AdminService {
+	/**
+	 * 日志组件
+	 */
+	protected static Logger logger = LoggerFactory.getLogger(AdminService.class);
+	/**
+	 * 缓存访问对象
+	 *
+	 * 缓存中内容为：ID
+	 */
+	@Resource(name = "cache")
+	protected ITimeLimitedCache<String, Integer> cache;
 	/**
 	 * 范例组件
 	 */
@@ -20,8 +35,27 @@ public class AdminService {
 	private AdminBean adminBean;
 
 
-	private static Logger log = Logger.getLogger(AdminService.class.getName());
+	/**
+	 * 根据会话ID获取管理员ID
+	 *
+	 * @param aId 会话ID
+	 * @return 管理员ID
+	 */
+	public Integer fetchAdminId(String aId) {
+		return cache.get("aid-" + aId);
+	}
 
+	/**
+	 * 保存会话
+	 *
+	 * @param adminId 管理员ID
+	 * @return 会话ID
+	 */
+	public String saveSession(int adminId) {
+		String aId = Serial.makeLocalID();
+		cache.set("aid-" + aId, adminId, 1000 * 60 * 24);
+		return aId;
+	}
 
 	/**
 	 * 根据ID加载AdminEntity
@@ -33,12 +67,11 @@ public class AdminService {
 		return adminBean.load(id);
 	}
 
-
 	/**
 	 * 根据飞行票ID和购买状态字段查询record
 	 *
-	 * @return AdminEntity
 	 * @Param userName
+	 * @return AdminEntity
 	 */
 	public AdminEntity findByUserName(String userName) {
 		return adminBean.findByUserName(userName);
@@ -47,7 +80,6 @@ public class AdminService {
 	/**
 	 * 新增管理用户
 	 *
-	 * @return void
 	 * @Param adminEntity
 	 */
 	public void addAdmin(AdminEntity adminEntity) {
@@ -58,7 +90,6 @@ public class AdminService {
 	/**
 	 * 更新管理用户
 	 *
-	 * @return void
 	 * @Param adminEntity
 	 */
 	public void updateAdmin(AdminEntity adminEntity) {
@@ -69,12 +100,9 @@ public class AdminService {
 	/**
 	 * 删除管理用户
 	 *
-	 * @return void
 	 * @Param id
 	 */
 	public void deleteAdmin(Integer id) {
 		adminBean.deleteAdmin(id);
 	}
-
-
 }
